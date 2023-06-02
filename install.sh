021206646b25071410267696982657a1d862d377
Matt Quinn <matt@mattjquinn.com>
2023-06-02 07:42:52 -0400

    Update the install script to handle any dotfile in dotfiles/, ignoring any dotfiles that already exist as a file or link.

--- install.sh
+++ install.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
+# TODO(collins): add ~/.config/nvim/init.vim to dotfiles/ in this git repo
+# TODO(collins): add ~/.config/X11 to dotfiles/ in this git repo
+# TODO(collins): add ~/.vim to dotfiles/ in this git repo
+# TODO(collins): add ~/.vimrc to dotfiles/ in this git repo
+
 # This script will create links for the dotfiles.
 
-##########
-#  nvim  #
-##########
-mkdir -p "$HOME/.config/nvim"
-mkdir -p "$HOME/.config/nvim/undo"
-ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim/init.vim"
+# Step 1: ensure all necessary directories exist before linking files.
+for dir in $(find dotfiles/ -mindepth 1 -type d); do
+	target="$HOME/${dir#dotfiles/}"
+	echo "INFO: Ensuring directory exists: $target"
+	mkdir -p $target
+done
 
-
-##############
-# Xresources #
-##############
-ln -sfn "$HOME/dotfiles/X11" "$HOME/.config"
-
-
-########
-#  vim #
-########
-ln -s "$HOME/dotfiles/vim/.vim" "$HOME/.vim"
-ln -s "$HOME/dotfiles/vim/.vimrc" "$HOME/.vimrc"
+# Step 2: create links for all dotfiles
+for file in $(find dotfiles/ -mindepth 1 -type f); do
+	source=$PWD/$file
+	target="$HOME/${file#dotfiles/}"
+	# This if statement could be replaced by passing -f to ln,
+	# but doing so could accidentally delete a dotfile that hasn't been
+	# added to this repo yet, which is why -f is not used, and instead
+	# a warning is printed to inform the user about the situation.
+	if [ -e "$target" ]; then
+		if [ ! -L "$target" ]; then
+			echo "WARN: $target exists but is not a symlink, skipping it."
+		else
+			echo "INFO: $target is already a symlink, skipping it."
+		fi
+	else
+		echo "INFO: linking $source to $target"
+		ln -s $source $target
+	fi
+done
