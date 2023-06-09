cd307c6ac49b25046ab2d29e892f1c9e9d664f0a
colspan <collin@collindigm.com>
2023-06-09 05:07:23 +0300

    Nested loop iterating over each directory withing the repo and adding the symlink

--- install.sh
+++ install.sh
@@ -9,27 +9,29 @@
 
 # Step 1: ensure all necessary directories exist before linking files.
 for dir in $(ls | find * -mindepth 1 -type d ! -name '.git' ! -name '.gitignore'); do
+
 	target="$HOME/${dir#dotfiles/}"
 	echo "INFO: Ensuring directory exists: $target"
 	mkdir -p $target
+
+    # Step 2: create links for all dotfiles
+    for file in $(find dir -mindepth 1 -type f); do
+        source=$PWD/$file
+        target="$HOME/${file#dotfiles/}"
+        # This if statement could be replaced by passing -f to ln,
+        # but doing so could accidentally delete a dotfile that hasn't been
+        # added to this repo yet, which is why -f is not used, and instead
+        # a warning is printed to inform the user about the situation.
+        if [ -e "$target" ]; then
+            if [ ! -L "$target" ]; then
+                echo "WARN: $target exists but is not a symlink, skipping it."
+            else
+                echo "INFO: $target is already a symlink, skipping it."
+            fi
+        else
+            echo "INFO: linking $source to $target"
+            ln -s $source $target
+        fi
+    done
 done
 
-# Step 2: create links for all dotfiles
-for file in $(find dotfiles/ -mindepth 1 -type f); do
-	source=$PWD/$file
-	target="$HOME/${file#dotfiles/}"
-	# This if statement could be replaced by passing -f to ln,
-	# but doing so could accidentally delete a dotfile that hasn't been
-	# added to this repo yet, which is why -f is not used, and instead
-	# a warning is printed to inform the user about the situation.
-	if [ -e "$target" ]; then
-		if [ ! -L "$target" ]; then
-			echo "WARN: $target exists but is not a symlink, skipping it."
-		else
-			echo "INFO: $target is already a symlink, skipping it."
-		fi
-	else
-		echo "INFO: linking $source to $target"
-		ln -s $source $target
-	fi
-done
