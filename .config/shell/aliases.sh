0bdfab161822cb7b6193bbc1a207440c96c1c2ce
colspan <collin@collindigm.com>
2023-06-30 23:41:44 +0300

    Package Manager AUR and pacman aliases added in the aliases script file

--- .config/shell/aliases.sh
+++ .config/shell/aliases.sh
@@ -2,3 +2,9 @@
 
 # Aliases
 alias lock='xtrlock -b'
+
+alias pupgrade='sudo pacman -Syu'   # Upgrade the System
+alias pclean='sudo pacman -Scc'     # Clean the cache
+alias yupgrade='yay -Syu'           # Fetch the database from the official repositories, update all your official packages and the packages from the AUR
+alias yclean='yay -Sc --aur'        # Clean AUR Cache
+alias pyclean='yay -Sc'             # Clean both the pacman and yay caches
