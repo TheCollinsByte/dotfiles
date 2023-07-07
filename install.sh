d40458d64163a55df04fd764d611cc2e72ccc291
colspan <collin@collindigm.com>
2023-07-07 22:48:16 +0300

    xinitrc: linked the file

--- install.sh
+++ install.sh
@@ -145,6 +145,7 @@ function backup_configs {
 function setup_symlinks {
     echo -e "\u001b[7m Setting up symlinks... \u001b[0m"
 
+    ln -sfnv "$PWD/.xinitrc" ~/
     ln -sfnv "$PWD/.config/ranger" ~/.config/
     ln -sfnv "$PWD/.config/fontconfig" ~/.config/
     ln -sfnv "$PWD/.config/bat" ~/.config/
