daaa9849bc07a4070da7807939f7f71856f19509
colspan <collin@collindigm.com>
2023-07-28 10:54:21 +0300

    intellij version update

--- .config/shell/aliases.sh
+++ .config/shell/aliases.sh
@@ -4,7 +4,7 @@
 alias q="exit"
 alias ls='ls --color=auto'
 
-alias intellij='wmname LG3D ; /opt/idea-IC-222.4167.29/bin/idea.sh'
+alias intellij='wmname LG3D ; /opt/idea-IC-231.9225.16/bin/idea.sh'
 alias aside='wmname LG3D ; /opt/android-studio/bin/studio.sh'
 
 alias l='xtrlock -b'
