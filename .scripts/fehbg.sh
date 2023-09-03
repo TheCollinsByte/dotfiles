e92d7c363f3d4bef7a92bc8a4e5ea822f9855beb
colspan <collin@collindigm.com>
2023-09-03 23:03:15 +0300

    More Script fixes

--- .scripts/fehbg.sh
+++ .scripts/fehbg.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 # Wallpaper Shuffle
-DIR="/home/$USER/.scripts/wallpapers"
+DIR="/home/$USER/./.config/wallpapers/"
 
 WAL="$(ls $DIR/*.png | shuf -n1)"
 
 cat $WAL > /home/$USER/.config/wallpapers/wallpaper.png
 
-feh --bg-scale /home/$USER/.config/wallpapers/wallpaper.png
+feh --no-fehbg --bg-scale /home/$USER/.config/wallpapers/wallpaper.png
