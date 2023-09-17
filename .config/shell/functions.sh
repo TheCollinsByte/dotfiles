81dd8bd4707d82945361d482a1b1fc12b8c0fb95
colspan <collin@collindigm.com>
2023-09-17 23:06:41 +0300

    fzf f(x) runs when tab is pressed after (,)

--- .config/shell/functions.sh
+++ .config/shell/functions.sh
@@ -63,3 +63,15 @@ ex() {
     fi
 }
 
+# Runs when tab is pressed after ,
+_fzf_comprun() {
+    local command=$1
+    shift
+
+    case "$command" in
+    cd) fzf "$@" --preview 'exa -TFl --group-directories-first --icons --git -L 2 --no-user {}' ;;
+    nvim) fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}' ;;
+    vim) fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}' ;;
+    *) fzf "$@" ;;
+    esac
+}
