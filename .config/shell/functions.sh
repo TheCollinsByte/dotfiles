88c1149667c131b0a174ce505d2436231d1511de
colspan <collin@collindigm.com>
2023-09-17 22:51:52 +0300

    Change directory and list files function implementation

--- .config/shell/functions.sh
+++ .config/shell/functions.sh
@@ -2,5 +2,12 @@
 
 # Change directory and view the contents at the same time
 function cl() {
-
+	DIR="$*";
+		# if no DIR given, go home
+		if [ $# -lt 1 ]; then 
+			DIR=$HOME;
+		fi;
+		builtin cd "${DIR}" && \
+		# preferred ls command
+		ls -F --color=auto
 }
