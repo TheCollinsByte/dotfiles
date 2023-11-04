cf83270b3f0059ca7daa3db7f2f0e2eb38c85fb9
colspan <collin@collindigm.com>
2023-11-04 21:40:29 +0300

    efmls nvim lspconfig, autopairs: Plugin configuration and Installation

---
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,82 @@
+local config = function()
+	require("neoconf").setup({})
+
+	local lspconfig = require("lspconfig")
+
+	local signs = { Error = " ", Warn = " ", Hint = "ﴞ ", Info = "" }
+
+	for type, icon in pairs(signs) do
+		local hl = "DiagnosticSign" .. type
+		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+	end
+
+	-- lua
+	lspconfig.lua_ls.setup({
+		capabilities = capabilities,
+		on_attach = on_attach,
+		settings = { -- Custom settings for lua
+			Lua = {
+				-- make the language server recognize "vim" global
+				diagnostics = {
+					globals = { "vim" },
+				},
+				workspace = {
+					-- make language server aware of runtime files
+					library = {
+						[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+						[vim.fn.stdpath("config") .. "/lua"] = true,
+					},
+				},
+			},
+		},
+	})
+
+	local luacheck = require("efmls-configs.linters.luacheck")
+	local stylua = require("efmls-configs.formatters.stylua")
+
+	-- configure efm server
+	lspconfig.efm.setup({
+		filetypes = {
+			"lua",
+		},
+		init_options = {
+			documentFormatting = true,
+			documentRangeFormatting = true,
+			hover = true,
+			documentSymbol = true,
+			codeAction = true,
+			completion = true,
+		},
+		settings = {
+			languages = {
+				lua = { luacheck, stylua },
+			},
+		},
+	})
+
+	-- Format on Save
+	local lsp_fmt_group = vim.api.nvim_create_augroup("LspFormattingGroup", {})
+	vim.api.nvim_create_autocmd("BufWritePost", {
+		group = lsp_fmt_group,
+		callback = function()
+			local efm = vim.lsp.get_active_clients({ name = "efm" })
+
+			if vim.tbl_isempty(efm) then
+				return
+			end
+
+			vim.lsp.buf.format({ name = "efm" })
+		end,
+	})
+end
+
+return {
+	"neovim/nvim-lspconfig",
+	config = config,
+	lazy = false,
+	dependencies = {
+		"windwp/nvim-autopairs",
+		"williamboman/mason.nvim",
+		"creativenull/efmls-configs-nvim",
+	},
+}
