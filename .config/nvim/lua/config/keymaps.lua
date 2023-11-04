22a42522341116fbc572039ac926227157801d5a
colspan <collin@collindigm.com>
2023-11-04 16:16:28 +0300

    Comment nvim plugin configuration

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -16,3 +16,11 @@ keymap.set("n", "<C-l>", "<C-w>l", opts)  -- Navigate Right
 keymap.set("n", "<leader>sv", ":vsplit<CR>", opts)          -- Split Vertically
 keymap.set("n", "<leader>sh", ":split<CR>", opts)           -- Split Horizontally
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>", opts) -- Toggle Minimize
+
+-- Indenting
+keymap.set("v", "<", "<gv")
+keymap.set("v", ">", ">gv")
+
+-- Comments
+vim.api.nvim_set_keymap("n", "<C-_>", "gcc", { noremap = false })
+vim.api.nvim_set_keymap("v", "<C-_>", "gcc", { noremap = false })
