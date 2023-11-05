b30b12edc9a28a0899e8115f1cc90c0262316bfd
colspan <collin@collindigm.com>
2023-11-05 12:03:43 +0300

    Configured tsserver, eslint and pretierd as the formatter

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,7 +2,7 @@ local on_attach = require("util.lsp").on_attach
 
 local config = function()
 	require("neoconf").setup({})
-  local cmp_nvim_lsp = require("cmp_nvim_lsp")
+	local cmp_nvim_lsp = require("cmp_nvim_lsp")
 	local lspconfig = require("lspconfig")
 
 	local signs = { Error = " ", Warn = " ", Hint = "", Info = "" }
@@ -12,7 +12,7 @@ local config = function()
 		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 	end
 
-  local capabilities = cmp_nvim_lsp.default_capabilities()
+	local capabilities = cmp_nvim_lsp.default_capabilities()
 
 	-- lua
 	lspconfig.lua_ls.setup({
@@ -52,16 +52,31 @@ local config = function()
 		},
 	})
 
+	-- Typescript
+	lspconfig.tsserver.setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+		filetypes = {
+			"typescript",
+		},
+		root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", ".git"),
+	})
+
 	local luacheck = require("efmls-configs.linters.luacheck")
 	local stylua = require("efmls-configs.formatters.stylua")
+
 	local flake8 = require("efmls-configs.linters.flake8")
 	local black = require("efmls-configs.formatters.black")
+
+	local eslint_d = require("efmls-configs.linters.eslint_d")
+	local prettierd = require("efmls-configs.formatters.prettier_d")
 
 	-- configure efm server
 	lspconfig.efm.setup({
 		filetypes = {
 			"lua",
 			"python",
+			"typescript",
 		},
 		init_options = {
 			documentFormatting = true,
@@ -75,6 +90,7 @@ local config = function()
 			languages = {
 				lua = { luacheck, stylua },
 				python = { flake8, black },
+				typescript = { eslint_d, prettierd },
 			},
 		},
 	})
@@ -103,7 +119,7 @@ return {
 		"windwp/nvim-autopairs",
 		"williamboman/mason.nvim",
 		"creativenull/efmls-configs-nvim",
-    "hrsh7th/nvim-cmp",
+		"hrsh7th/nvim-cmp",
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-nvim-lsp",
 	},
