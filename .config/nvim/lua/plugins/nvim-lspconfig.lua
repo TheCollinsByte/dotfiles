65a637be67a4c72484b9f8367158f199cd7efef7
colspan <collin@collindigm.com>
2023-11-05 12:10:21 +0300

    Auto save moved to autocmds file for separation of concerns

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -95,20 +95,6 @@ local config = function()
 		},
 	})
 
-	-- Format on Save
-	local lsp_fmt_group = vim.api.nvim_create_augroup("LspFormattingGroup", {})
-	vim.api.nvim_create_autocmd("BufWritePost", {
-		group = lsp_fmt_group,
-		callback = function()
-			local efm = vim.lsp.get_active_clients({ name = "efm" })
-
-			if vim.tbl_isempty(efm) then
-				return
-			end
-
-			vim.lsp.buf.format({ name = "efm" })
-		end,
-	})
 end
 
 return {
