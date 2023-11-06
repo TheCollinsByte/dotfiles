160dce067274e3962a42831828318323e64d8a45
colspan <collin@collindigm.com>
2023-11-06 13:56:43 +0300

    setting Cmd tag within Telescope Command

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -36,12 +36,11 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = config,
 	keys = {
-		keymap.set("n", "<leader>fk", "Telescope keymaps<CR>"),
-		keymap.set("n", "<leader>fh", "Telescope help_tags<CR>"),
-		keymap.set("n", "<leader>ff", "Telescope find_files<CR>"),
-		keymap.set("n", "<leader>fa", "Telescope <CR>"),
-		keymap.set("n", "<leader>fg", "Telescope live_grep<CR>"),
-		keymap.set("n", "<leader>fb", "Telescope buffers<CR>"),
+		keymap.set("n", "<leader>fk", "<Cmd>Telescope keymaps<CR>"),
+		keymap.set("n", "<leader>fh", "<Cmd>Telescope help_tags<CR>"),
+		keymap.set("n", "<leader>ff", "<Cmd>Telescope find_files<CR>"),
+		keymap.set("n", "<leader>fa", "<Cmd>Telescope <CR>"),
+		keymap.set("n", "<leader>fg", "<Cmd>Telescope live_grep<CR>"),
+		keymap.set("n", "<leader>fb", "<Cmd>Telescope buffers<CR>"),
 	},
 }
-
