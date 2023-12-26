4e7ea884b8e3909420b41bee9dad7a86a6313f00
colspan <collin@collindigm.com>
2023-12-26 11:04:08 +0300

    Main Options configuration initialized

--- .config/nvim/lua/plugins/onedark.lua
+++ .config/nvim/lua/plugins/onedark.lua
@@ -1,15 +1,16 @@
 return {
-	"navarasu/onedark.nvim",
-	lazy = false,
-	priority = 999,
-	config = function()
-		vim.cmd("colorscheme onedark")
-    -- Main options --
-    style = 'deep', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-    transparent = true, -- Show/hide background
-    term_colors = false, -- Change terminal color as per the selected theme style
-    ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
-    cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
-	end,
+    "navarasu/onedark.nvim",
+    lazy = false,
+    priority = 999,
+    config = function()
+        vim.cmd("colorscheme onedark")
+
+        -- Main options --
+        vim.g.onedark_style = 'deep' -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+        vim.g.onedark_transparent = true -- Show/hide background
+        vim.g.onedark_term_colors = false -- Change terminal color as per the selected theme style
+        vim.g.onedark_ending_tildes = false -- Show the end-of-buffer tildes. By default they are hidden
+        vim.g.onedark_cmp_itemkind_reverse = false -- Reverse item kind highlights in cmp menu
+    end,
 }
 
