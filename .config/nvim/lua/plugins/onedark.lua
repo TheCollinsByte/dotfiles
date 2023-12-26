d75ffeae5a44cc6f2de4b399b1454ced25964a72
colspan <collin@collindigm.com>
2023-12-26 11:14:23 +0300

    More colorscheme configurations added

--- .config/nvim/lua/plugins/onedark.lua
+++ .config/nvim/lua/plugins/onedark.lua
@@ -20,6 +20,35 @@ return {
             strings = 'none',
             variables = 'none',
         }
+
+        -- Lualine options --
+        vim.g.onedark_lualine = {
+            transparent = true, -- Lualine center bar transparency
+        }
+
+        -- Plugins Config --
+        vim.g.onedark_diagnostics = {
+            darker = false, -- Darker colors for diagnostic
+            undercurl = true, -- Use undercurl instead of underline for diagnostics
+            background = false, -- Use background color for virtual text
+        }
+
+        -- Custom Highlights --
+        vim.g.onedark_colors = {
+            -- bright_orange = "#ff8800",
+            -- green = '#00ffaa',
+        }
+        vim.g.onedark_highlights = {
+            -- ["@keyword"] = { fg = '$green' },
+            -- ["@string"] = { fg = '$bright_orange', bg = '#00ff00', fmt = 'bold' },
+            -- ["@function"] = { fg = '#0000ff', sp = '$cyan', fmt = 'underline,italic' },
+            -- ["@function.builtin"] = { fg = '#0059ff' }
+        }
+
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+        vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
     end,
 }
 
