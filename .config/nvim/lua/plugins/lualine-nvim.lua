2b845f8ed12b8e4c41fbfe7891e005cdd752cfb0
colspan <collin@collindigm.com>
2023-12-26 10:47:10 +0300

    Lua Line Update to the current colorscheme

--- .config/nvim/lua/plugins/lualine-nvim.lua
+++ .config/nvim/lua/plugins/lualine-nvim.lua
@@ -1,5 +1,5 @@
 local config = function() 
-  local theme = require("lualine.themes.nightfox")
+  local theme = require("lualine.themes.onedark")
   theme.normal.c.bg = nil
 
   require("lualine").setup({
