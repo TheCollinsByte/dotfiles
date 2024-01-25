48e73d1baf2397de86c1a2fb59d16f52c262eb2e
Collo <collo@fastmail.com>
2024-01-26 00:21:29 +0300

    system monitor package htop

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ system_kind=""
 function install_arch {
     packages_to_install=(
         curl git vim neovim tmux openssh htop unzip bat lsd calibre okular
-        bluez bluez-utils cmus fzf xtrlock fd feh aws-cli yay ripgrep
+        bluez bluez-utils cmus fzf xtrlock fd feh aws-cli yay ripgrep htop
    )
 
    for package in "${packages_to_install[@]}"; do
@@ -56,7 +56,7 @@ function install_arch {
 
 function install_debian {
     packages_to_install=(
-        curl git vim neovim tmux openssh htop unzip bat lsd
+        curl git vim neovim tmux openssh htop unzip bat lsd htop
         cmus fzf xtrlock fd-find feh awscli ripgrep calibre okular
    )
 
