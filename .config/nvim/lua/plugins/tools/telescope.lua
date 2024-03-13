f466fc4c3e4771503502c9916c29947f2efd9039
colspan <collin@collindigm.com>
2024-03-13 23:46:20 +0300

    Telescope configuration updated

--- .config/nvim/lua/plugins/tools/telescope.lua
+++ .config/nvim/lua/plugins/tools/telescope.lua
@@ -2,3 +2,124 @@ local status_ok, telescope = pcall(require, 'telescope')
 if not status_ok then
     return
 end
+
+local actions = require('telescope.actions')
+local icons = require('lib.icons')
+
+telescope.setup({
+    defaults = {
+        layout_config = {
+            height = 0.8,
+            width = 0.9,
+            prompt_position = 'top',
+            bottom_pane = {
+                height = 0.5,
+                preview_width = 0.6,
+                preview_cutoff = 120,
+            },
+            center = {
+                height = 0.4,
+                preview_cutoff = 40,
+            },
+            cursor = {
+                preview_cutoff = 40,
+                preview_width = 0.6,
+            },
+            horizontal = {
+                preview_width = 0.6,
+                preview_cutoff = 120,
+            },
+            vertical = {
+                preview_cutoff = 40,
+            },
+        },
+
+        prompt_prefix = icons.ui.Telescope .. icons.ui.ChevronRight,
+        selection_caret = icons.ui.Play,
+        multi_icon = icons.ui.Check,
+        path_display = { 'smart' },
+        sorting_strategy = 'ascending',
+
+        mappings = {
+            i = {
+                ['<esc>'] = actions.close,
+                ['<C-n>'] = actions.cycle_history_next,
+                ['<C-p>'] = actions.cycle_history_prev,
+
+                ['<C-j>'] = actions.move_selection_next,
+                ['<C-k>'] = actions.move_selection_previous,
+
+                ['<C-c>'] = actions.close,
+
+                ['<Down>'] = actions.move_selection_next,
+                ['<Up>'] = actions.move_selection_previous,
+
+                ['<CR>'] = actions.select_default,
+                ['<C-x>'] = actions.select_horizontal,
+                ['<C-v>'] = actions.select_vertical,
+                ['<C-t>'] = actions.select_tab,
+                ['<C-s>'] = flash,
+
+                ['<C-u>'] = actions.preview_scrolling_up,
+                ['<C-d>'] = actions.preview_scrolling_down,
+
+                ['<PageUp>'] = actions.results_scrolling_up,
+                ['<PageDown>'] = actions.results_scrolling_down,
+
+                ['<Tab>'] = actions.toggle_selection + actions.move_selection_worse,
+                ['<S-Tab>'] = actions.toggle_selection + actions.move_selection_better,
+                ['<C-q>'] = actions.send_to_qflist + actions.open_qflist,
+                ['<M-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
+                ['<C-l>'] = actions.complete_tag,
+            },
+
+            n = {
+                ['q'] = actions.close,
+                ['<esc>'] = actions.close,
+                ['<CR>'] = actions.select_default,
+                ['<C-x>'] = actions.select_horizontal,
+                ['<C-v>'] = actions.select_vertical,
+                ['<C-t>'] = actions.select_tab,
+                ['s'] = flash,
+
+                ['<Tab>'] = actions.toggle_selection + actions.move_selection_worse,
+                ['<S-Tab>'] = actions.toggle_selection + actions.move_selection_better,
+                ['<C-q>'] = actions.send_to_qflist + actions.open_qflist,
+                ['<M-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
+
+                ['j'] = actions.move_selection_next,
+                ['k'] = actions.move_selection_previous,
+                ['H'] = actions.move_to_top,
+                ['M'] = actions.move_to_middle,
+                ['L'] = actions.move_to_bottom,
+
+                ['<Down>'] = actions.move_selection_next,
+                ['<Up>'] = actions.move_selection_previous,
+                ['gg'] = actions.move_to_top,
+                ['G'] = actions.move_to_bottom,
+
+                ['<C-u>'] = actions.preview_scrolling_up,
+                ['<C-d>'] = actions.preview_scrolling_down,
+
+                ['<PageUp>'] = actions.results_scrolling_up,
+                ['<PageDown>'] = actions.results_scrolling_down,
+
+                ['?'] = actions.which_key,
+            },
+        },
+    },
+
+    pickers = {
+        find_files = {
+				previewer = false,
+				hidden = true,
+		},
+        live_grep = {
+				previewer = false
+		},
+        buffers = {
+				previewer = false
+		},
+    },
+})
+
