60234e914d9c4962b55897171f2b34d90fef3246
colspan <collin@collindigm.com>
2024-03-13 23:44:34 +0300

    Dashboard configuration updated

--- .config/nvim/lua/plugins/ui/dashboard.lua
+++ .config/nvim/lua/plugins/ui/dashboard.lua
@@ -6,61 +6,72 @@ end
 local icons = require('lib.icons')
 
 dashboard.setup({
-	theme = 'doom', --  theme is doom and hyper default is hyper
-	disable_move = false, --  defualt is false disable move keymap for hyper
-    	shortcut_type = 'letter', --  shorcut type 'letter' or 'number'
-    	change_to_vcs_root = false, -- default is false,for open file in hyper mru. it will change to the root of vcs
-    	hide = {
-        	statusline = true, -- hide statusline default is true
-        	tabline = false, -- hide the tabline
-        	winbar = false, -- hide winbar
-    	},
-	config = {
-		week_header = {
-            		enable = true,
-            		append = { '', icons.ui.Circle .. ' TheOddagen: Learn. Make, Explore! ' .. icons.ui.Circle },
-        	},
-        	packages = { enable = true }, -- show how many plugins neovim loaded
-        	project = { enable = true, limit = 8 },
-        	mru = { limit = 8 },
-        	shortcut = {
-        	    {
-        	        desc = icons.ui.Package .. 'Plugins',
-        	        group = '@property',
-        	        action = 'Lazy',
-        	        key = 'p',
-        	    },
-        	    {
-        	        desc = icons.ui.NeoVim .. 'Config',
-        	        group = '@property',
-        	        action = 'e ~/.config/nvim/lua/plugins/list.lua',
-        	        key = 'c',
-        	    },
-        	    {
-        	        desc = icons.documents.OpenFolder .. 'Files',
-        	        group = 'Label',
-        	        action = 'lua require("telescope").extensions.menufacture.find_files()',
-        	        key = 'f',
-        	    },
-        	    {
-        	        desc = icons.ui.Search .. 'Search',
-        	        group = 'Label',
-        	        action = 'lua require("telescope").extensions.menufacture.live_grep()',
-        	        key = 's',
-        	    },
-        	    {
-        	        desc = icons.ui.Note .. 'Notes',
-        	        group = 'Label',
-        	        action = 'Telekasten goto_today',
-        	        key = 'n',
-        	    },
-        	    {
-        	        desc = icons.ui.Power,
-        	        group = 'Action',
-        	        action = 'quit',
-        	        key = 'q',
-        	    },
-        	},
-        	footer = { '', icons.ui.Heart .. ' Happiness is a state of mind. ' .. icons.ui.Heart },
-	}
+    theme = 'hyper',
+    disable_move = false,
+    shortcut_type = 'letter',
+    change_to_vcs_root = true,
+    hide = {
+        statusline = true,
+        tabline = false,
+        winbar = false,
+    },
+    config = {
+        week_header = {
+            enable = true,
+            append = { '', icons.ui.Circle .. ' TheOddagen: There is always one more bug to fix. ' .. icons.ui.Circle },
+        },
+        packages = { enable = true },
+        project = { enable = true, limit = 8 },
+        mru = { limit = 8 },
+        shortcut = {
+            {
+                desc = icons.ui.Package .. 'Mason',
+                group = '@property',
+                action = 'Mason',
+                key = 'm',
+            },
+            {
+                desc = icons.ui.Sleep .. 'Lazy',
+                group = '@property',
+                action = 'Lazy',
+                key = 'p',
+            },
+            {
+                desc = icons.documents.OpenFolder .. 'Files',
+                group = 'Label',
+                action = 'lua require("telescope").extensions.menufacture.find_files()',
+                key = 'f',
+            },
+            {
+                desc = icons.ui.Search .. 'Search',
+                group = 'Label',
+                action = 'lua require("telescope").extensions.menufacture.live_grep()',
+                key = 'g',
+            },
+            {
+                desc = icons.ui.Note .. 'Todo',
+                group = 'Label',
+                action = 'Tdo',
+                key = 'd',
+            },
+            {
+                desc = icons.ui.Power,
+                group = 'Action',
+                action = 'quit',
+                key = 'q',
+            },
+        },
+        footer = { '', icons.ui.Heart .. ' My LeetCode and codeforces rating is not nearly as impressive as how many times I put vim on your mom\'s computer. ' .. icons.ui.Heart },
+    },
 })
+
+-- Show dashboard when lazy closes
+if vim.o.filetype == 'lazy' then
+    vim.cmd.close()
+    vim.api.nvim_create_autocmd('User', {
+        pattern = 'DashboardLoaded',
+        callback = function()
+            require('lazy').show()
+        end,
+    })
+end
