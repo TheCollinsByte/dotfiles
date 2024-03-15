2c304481e43155631f5c629d0b23e821e1fbfb86
colspan <collin@collindigm.com>
2024-03-15 20:28:15 +0300

    more keymaps initialized

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,7 +1,5 @@
 local keymap = vim.keymap
 
--- TODO: Helper Function use to set keymap
-
 local opts = { noremap = true, silent = true }
 
 
@@ -9,10 +7,11 @@ local opts = { noremap = true, silent = true }
 -- normal_mode = 'n', insert_mode = 'i', visual_mode = 'v',
 -- visual_block_mode = 'x', term_mode = 't', command_mode = 'c',
 
--- Space as leader
-keymap.set('n', '<Space>', '', opts)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+-- Directory Navigation
+keymap.set("n", "<leader>m", ":NvimTreeFocus<CR>", { noremap = true, silent = true })
+keymap.set("n", "<leader>f", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+
+
 
 -- Disable netrw
 vim.g.loaded_netrw = 1
@@ -20,10 +19,26 @@ vim.g.loaded_netrwPlugin = 1
 
 
 -- Split/Pane and Window Navigation
+keymap.set("n", "<C-h>", "<C-w>h", opts) -- Navigate Left
+keymap.set("n", "<C-j>", "<C-w>j", opts) -- Navigate Down
+keymap.set("n", "<C-k>", "<C-w>k", opts) -- Navigate Up
+keymap.set("n", "<C-l>", "<C-w>l", opts) -- Navigate Right
 
+keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts) -- Navigate Left
+keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts) -- Navigate Down
+keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts) -- Navigate Up
+keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts) -- Navigate Right
+
+keymap.set("n", "<C-h>", ":TmuxNavigateLeft<CR>", opts) -- Navigate Left
+keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>", opts) -- Navigate Down
+keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>", opts) -- Navigate Up
+keymap.set("n", "<C-l>", ":TmuxNavigateRight<CR>", opts) -- Navigate Right
 
 
 -- Window Management
+keymap.set("n", "<leader>sv", ":vsplit<CR>", opts) -- Split Vertically
+keymap.set("n", "<leader>sh", ":split<CR>", opts) -- Split Horizontally
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>", opts) -- Toggle Minimize
 
 
 
@@ -33,4 +48,5 @@ keymap.set('v', '>', '>gv')
 
 
 -- Comments
-
+vim.api.nvim_set_keymap("n", "<C-_>", "gcc", { noremap = false })
+vim.api.nvim_set_keymap("v", "<C-_>", "gcc", { noremap = false })
